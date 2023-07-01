ff09ddf872be2f1d8844197130c7c7b7a86ec70f
Samuel Akuma <usakuma@uwaterloo.ca>
2023-07-01 00:57:23 -0400

    Initial commit

---
+++ src/main/java/dev/phoenixtype/rekognitiondemo/RekognitionDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.phoenixtype.rekognitiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekognitionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RekognitionDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/phoenixtype/rekognitiondemo/RekognitionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.phoenixtype.rekognitiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RekognitionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
